b8dec90d88c70c998ab6dbf92c65626ae10e8cff
Yijin Ro <yri.carmine@gmail.com>
2023-02-27 13:12:37 -0500

    front page complete

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './home.css'
 
 const Home = () => {
     return (
     <div className='homeBody'>
     {/* this is for the first line */}
       <div className='firstBlock'>
-          <h2>Creative</h2>
+          <h2 className='creative'>Creative</h2>
           <div className='firstparagraph'>
             <p>Developer</p>
             <p>Designer</p>
@@ -13,15 +14,16 @@ const Home = () => {
           </div>
       </div>
       <div className='secondBlock'>
-        <h2>Developer</h2>
+        <h2 className='developer'>Developer</h2>
       </div>
       <div className='thirdBLock'>
-        <p>I am a software developer with design background based in East Coast</p>
-        <p>I bring an eye for design aesthetics, attention to detail</p>
-        <p>and communication skills to share ideas with people from a wide range of backgrounds</p>
+        <h2 className='star'>*</h2>
+        <div className='thirdBlockcontent'>
+        <p>I am a software developer with design background based in East Coast. I bring an eye for design aesthetics, attention to detail and communication skills to share ideas with people from a wide range of backgrounds</p>
+        </div>
       </div>
       <div className='fourthBlock'>
-          <h2>Designer</h2>
+          <h2 className='designer'>Designer</h2>
       </div>
     </div>
     );
